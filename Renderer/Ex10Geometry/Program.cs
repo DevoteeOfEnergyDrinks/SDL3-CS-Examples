214edb6ffb1b476ab931903158a3bfb3a1a0a805
EnergyDrinkDevotee <EnergyDrinkDevotee@proton.me>
2026-07-11 22:29:51 +0200

    Finished RendererEx10

--- Renderer/Ex10Geometry/Program.cs
+++ Renderer/Ex10Geometry/Program.cs
@@ -6,6 +6,8 @@
 * This code is public domain. Feel free to use it for any purpose!
 * This code is a port of the official SDL3 examples
 */
+using System.Runtime.InteropServices;
+
 internal class Program
 {
     // These delegates map our C# methods to the internal SDL3 lifecycle events.
@@ -19,6 +21,13 @@ internal class Program
     // These variables hold the memory addresses of the window and the renderer.
     public static IntPtr window = IntPtr.Zero;
     public static IntPtr renderer = IntPtr.Zero;
+    public static IntPtr texture = IntPtr.Zero;
+
+    static int textureWidth = 0;
+    static int textureHeight = 0;
+
+    const int WindowWidth = 640;
+    const int WindowHeigth = 480;
 
 
     private static void Main(string[] args)
@@ -44,21 +53,52 @@ internal class Program
     // This function runs once at startup.
     static AppResult AppInit(ref nint appstate, int argc, string[]? argv)
     {
-        SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
+        IntPtr surfacePtr = IntPtr.Zero;
+        string pngPath;
+
+        SetAppMetadata("Example Renderer Scaling Textures", "1.0", "com.example.renderer-geometry");
 
         if (!Init(InitFlags.Video))
         {
             Log($"Couldn't initialize SDL: {GetError()}");
             return AppResult.Failure;
         }
-        
-        if (!CreateWindowAndRenderer("examples/renderer/clear", 640, 480, WindowFlags.Resizable, out window, out renderer))
+
+        if (!CreateWindowAndRenderer("examples/renderer/geometry", WindowWidth, WindowHeigth, WindowFlags.Resizable, out window, out renderer))
         {
             Log($"Couldn't create window/renderer: {GetError()}");
             return AppResult.Failure;
         }
 
-        SetRenderLogicalPresentation(renderer, 640, 480, RendererLogicalPresentation.Letterbox);
+        SetRenderLogicalPresentation(renderer, WindowWidth, WindowHeigth, RendererLogicalPresentation.Letterbox);
+
+        // Textures are pixel data that we upload to the video hardware for fast drawing. 
+        // Lots of 2D engines refer to these as "sprites." 
+        // We'll do a static texture (upload once, draw many times) with data from a bitmap file.
+
+        // SDL_Surface is pixel data the CPU can access. SDL_Texture is pixel data the GPU can access.
+        // Load a .png into a surface, move it to a texture from there. 
+        pngPath = GetBasePath() + "assets/sample.png";  // build a string of the full file path
+        surfacePtr = LoadPNG(pngPath);
+        if (surfacePtr == IntPtr.Zero)
+        {
+            Log($"Couldn't load bitmap: {GetError()}");
+            return AppResult.Failure;
+        }
+
+        Surface surface = Marshal.PtrToStructure<Surface>(surfacePtr);
+
+        textureWidth = surface.Width;
+        textureHeight = surface.Height;
+
+        texture = CreateTextureFromSurface(renderer, surfacePtr);
+        if (texture == IntPtr.Zero)
+        {
+            Log($"Couldn't create static texture: {GetError()}");
+            return AppResult.Failure;
+        }
+
+        DestroySurface(surfacePtr);  // done with this, the texture has a copy of the pixels now.
 
         return AppResult.Continue;  // carry on with the program!
     }
@@ -78,29 +118,91 @@ internal class Program
     // This function runs once per frame, and is the heart of the program.
     static AppResult AppIterate(nint appstate)
     {
+        // Adding a delay to stop very high FPS
         Delay(6);
-        double now = GetTicks() / 1000.0f;  // convert from milliseconds to seconds.
+        ulong now = GetTicks();
 
-        // choose the color for the frame we will draw. The sine wave trick makes it fade between colors smoothly.
-        float red = (float)(0.5f + 0.5f * Math.Sin(now));
-        float green = (float)(0.5f + 0.5f * Math.Sin(now + Math.PI * 2 / 3));
-        float blue = (float)(0.5f + 0.5f * Math.Sin(now + Math.PI * 4 / 3));
+        // we'll have the triangle grow and shrink over a few seconds. */
+        float direction = ((now % 2000) >= 1000) ? 1.0f : -1.0f;
+        float scale = ((float)(((int)(now % 1000)) - 500) / 500.0f) * direction;
+        float size = 200.0f + (200.0f * scale);
 
-        SetRenderDrawColorFloat(renderer, red, green, blue, 1.0f);  // new color, full alpha
+        Vertex[] vertices = new Vertex[4];
+        int i;
 
-        // clear the window to the draw color.
-        RenderClear(renderer);
+        // as you can see from this, rendering draws over whatever was drawn before it.
+        SetRenderDrawColor(renderer, 0, 0, 0, byte.MaxValue);  // black, full alpha
+        RenderClear(renderer);  // start with a blank canvas.
 
-        // put the newly-cleared rendering on the screen.
-        RenderPresent(renderer);
+        // Draw a single triangle with a different color at each vertex. Center this one and make it grow and shrink.
+        // You always draw triangles with this, but you can string triangles together to form polygons.
+        vertices[0].Position.X = ((float)WindowWidth) / 2.0f;
+        vertices[0].Position.Y = (((float)WindowHeigth) - size) / 2.0f;
+        vertices[0].Color.R = 1.0f;
+        vertices[0].Color.A = 1.0f;
+        vertices[1].Position.X = (((float)WindowWidth) + size) / 2.0f;
+        vertices[1].Position.Y = (((float)WindowHeigth) + size) / 2.0f;
+        vertices[1].Color.G = 1.0f;
+        vertices[1].Color.A = 1.0f;
+        vertices[2].Position.X = (((float)WindowWidth) - size) / 2.0f;
+        vertices[2].Position.Y = (((float)WindowHeigth) + size) / 2.0f;
+        vertices[2].Color.B = 1.0f;
+        vertices[2].Color.A = 1.0f;
 
-        return AppResult.Continue;  // carry on with the program!
+        RenderGeometry(renderer, IntPtr.Zero, vertices, 3, IntPtr.Zero, 0);
+
+        // you can also map a texture to the geometry! Texture coordinates go from 0.0f to 1.0f. 
+        // That will be the location in the texture bound to this vertex.
+        vertices[0].Position.X = 10.0f;
+        vertices[0].Position.Y = 10.0f;
+        vertices[0].Color.R = vertices[0].Color.G = vertices[0].Color.B = vertices[0].Color.A = 1.0f;
+        vertices[0].TexCoord.X = 0.0f;
+        vertices[0].TexCoord.Y = 0.0f;
+        vertices[1].Position.X = 150.0f;
+        vertices[1].Position.Y = 10.0f;
+        vertices[1].Color.R = vertices[1].Color.G = vertices[1].Color.B = vertices[1].Color.A = 1.0f;
+        vertices[1].TexCoord.X = 1.0f;
+        vertices[1].TexCoord.Y = 0.0f;
+        vertices[2].Position.X = 10.0f;
+        vertices[2].Position.Y = 150.0f;
+        vertices[2].Color.R = vertices[2].Color.G = vertices[2].Color.B = vertices[2].Color.A = 1.0f;
+        vertices[2].TexCoord.X = 0.0f;
+        vertices[2].TexCoord.Y = 1.0f;
+        RenderGeometry(renderer, texture, vertices, 3, IntPtr.Zero, 0);
+
+        // Did that only draw half of the texture? You can do multiple triangles sharing some vertices,
+        // using indices, to get the whole thing on the screen: */
+
+        // Let's just move this over so it doesn't overlap... */
+        for (i = 0; i < 3; i++)
+        {
+            vertices[i].Position.X += 450;
+        }
+
+        /* we need one more vertex, since the two triangles can share two of them. */
+        vertices[3].Position.X = 600.0f;
+        vertices[3].Position.Y = 150.0f;
+        vertices[3].Color.R = vertices[3].Color.G = vertices[3].Color.B = vertices[3].Color.A = 1.0f;
+        vertices[3].TexCoord.X = 1.0f;
+        vertices[3].TexCoord.Y = 1.0f;
+
+        // And an index to tell it to reuse some of the vertices between triangles...
+        {
+            // 4 vertices, but 6 actual places they used. Indices need less bandwidth to transfer and can reorder vertices easily!
+            int[] indices = [0, 1, 2, 1, 2, 3];
+            RenderGeometry(renderer, texture, vertices, 4, indices, indices.Length);
+        }
+
+        RenderPresent(renderer);  // put it all on the screen! */
+
+        return AppResult.Continue;
     }
 
 
     // This function runs once at shutdown.
     static void AppQuit(nint appstate, AppResult result)
     {
+        DestroyTexture(texture);
         // SDL will clean up the window/renderer for us.
     }
 }
