42ee5e05dba05204a0fb534a9124591c64814037
EnergyDrinkDevotee <EnergyDrinkDevotee@proton.me>
2026-07-14 20:22:49 +0200

    Finished RendererEx14

--- Renderer/Ex16AffineTextures/Program.cs
+++ Renderer/Ex16AffineTextures/Program.cs
@@ -1,11 +1,11 @@
 ﻿/*
-* This example code creates an SDL window and renderer, and then clears the
-* window to a different color every frame, so you'll effectively get a window
-* that's smoothly fading between colors.
+* This example creates an SDL window and renderer, and then draws a cube
+* using affine-transformed textures every frame.
 *
 * This code is public domain. Feel free to use it for any purpose!
-* This code is a port of the official SDL3 examples
 */
+using System.Runtime.InteropServices;
+
 internal class Program
 {
     // These delegates map our C# methods to the internal SDL3 lifecycle events.
@@ -19,7 +19,12 @@ internal class Program
     // These variables hold the memory addresses of the window and the renderer.
     public static IntPtr window = IntPtr.Zero;
     public static IntPtr renderer = IntPtr.Zero;
+    public static IntPtr texture = IntPtr.Zero;
 
+    static int textureWidth = 0;
+    static int textureHeight = 0;
+    const int WindowWidth = 640;
+    const int WindowHeight = 480;
 
     private static void Main(string[] args)
     {
@@ -44,21 +49,52 @@ internal class Program
     // This function runs once at startup.
     static AppResult AppInit(ref nint appstate, int argc, string[]? argv)
     {
-        SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
+        IntPtr surfacePtr = IntPtr.Zero;
+        string pngPath;
+
+        SetAppMetadata("Example Renderer Affine Textures", "1.0", "com.example.renderer-affine-textures");
 
         if (!Init(InitFlags.Video))
         {
             Log($"Couldn't initialize SDL: {GetError()}");
             return AppResult.Failure;
         }
-        
-        if (!CreateWindowAndRenderer("examples/renderer/clear", 640, 480, WindowFlags.Resizable, out window, out renderer))
+
+        if (!CreateWindowAndRenderer("examples/renderer/clear", WindowWidth, WindowHeight, WindowFlags.Resizable, out window, out renderer))
         {
             Log($"Couldn't create window/renderer: {GetError()}");
             return AppResult.Failure;
         }
 
-        SetRenderLogicalPresentation(renderer, 640, 480, RendererLogicalPresentation.Letterbox);
+        SetRenderLogicalPresentation(renderer, WindowWidth, WindowHeight, RendererLogicalPresentation.Letterbox);
+
+        // Textures are pixel data that we upload to the video hardware for fast drawing. 
+        // Lots of 2D engines refer to these as "sprites." 
+        // We'll do a static texture (upload once, draw many times) with data from a bitmap file.
+
+        // SDL_Surface is pixel data the CPU can access. SDL_Texture is pixel data the GPU can access.
+        // Load a .png into a surface, move it to a texture from there. 
+        pngPath = GetBasePath() + "assets/sample.png";  // build a string of the full file path
+        surfacePtr = LoadPNG(pngPath);
+        if (surfacePtr == IntPtr.Zero)
+        {
+            Log($"Couldn't load bitmap: {GetError()}");
+            return AppResult.Failure;
+        }
+
+        Surface surface = Marshal.PtrToStructure<Surface>(surfacePtr);
+
+        textureWidth = surface.Width;
+        textureHeight = surface.Height;
+
+        texture = CreateTextureFromSurface(renderer, surfacePtr);
+        if (texture == IntPtr.Zero)
+        {
+            Log($"Couldn't create static texture: {GetError()}");
+            return AppResult.Failure;
+        }
+
+        DestroySurface(surfacePtr);  // done with this, the texture has a copy of the pixels now.
 
         return AppResult.Continue;  // carry on with the program!
     }
@@ -79,19 +115,62 @@ internal class Program
     static AppResult AppIterate(nint appstate)
     {
         Delay(6);
-        double now = GetTicks() / 1000.0f;  // convert from milliseconds to seconds.
+        float x0 = 0.5f * WindowWidth;
+        float y0 = 0.5f * WindowHeight;
+        float px = Math.Min(WindowWidth, WindowHeight) / MathF.Sqrt(3.0f);
 
-        // choose the color for the frame we will draw. The sine wave trick makes it fade between colors smoothly.
-        float red = (float)(0.5f + 0.5f * Math.Sin(now));
-        float green = (float)(0.5f + 0.5f * Math.Sin(now + Math.PI * 2 / 3));
-        float blue = (float)(0.5f + 0.5f * Math.Sin(now + Math.PI * 4 / 3));
+        ulong now = GetTicks();
+        float rad = (((float)((int)(now % 2000))) / 2000.0f) * MathF.PI * 2;
+        float cos = MathF.Cos(rad);
+        float sin = MathF.Sin(rad);
+        float[] k = [3.0f / MathF.Sqrt(50.0f), 4.0f / MathF.Sqrt(50.0f), 5.0f / MathF.Sqrt(50.0f)];
+        float[] mat =
+        [
+             cos      + (1.0f-cos)*k[0]*k[0], -sin*k[2] + (1.0f-cos)*k[0]*k[1],  sin*k[1] + (1.0f-cos)*k[0]*k[2],
+             sin*k[2] + (1.0f-cos)*k[0]*k[1],  cos      + (1.0f-cos)*k[1]*k[1], -sin*k[0] + (1.0f-cos)*k[1]*k[2],
+            -sin*k[1] + (1.0f-cos)*k[0]*k[2],  sin*k[0] + (1.0f-cos)*k[1]*k[2],  cos      + (1.0f-cos)*k[2]*k[2],
+        ];
 
-        SetRenderDrawColorFloat(renderer, red, green, blue, 1.0f);  // new color, full alpha
+        float[] corners = new float[16];
+        int i;
 
-        // clear the window to the draw color.
+        for (i = 0; i < 8; i++)
+        {
+            float x = (i & 1) != 0 ? -0.5f : 0.5f;
+            float y = (i & 2) != 0 ? -0.5f : 0.5f;
+            float z = (i & 4) != 0 ? -0.5f : 0.5f;
+            corners[0 + 2 * i] = mat[0] * x + mat[1] * y + mat[2] * z;
+            corners[1 + 2 * i] = mat[3] * x + mat[4] * y + mat[5] * z;
+        }
+
+        SetRenderDrawColor(renderer, 0x42, 0x87, 0xf5, byte.MaxValue);  // light blue background.
         RenderClear(renderer);
 
-        // put the newly-cleared rendering on the screen.
+        for (i = 1; i < 7; i++)
+        {
+            int dir = 3 & ((i & 4) != 0 ? ~i : i);
+            int odd = (i & 1) ^ ((i & 2) >> 1) ^ ((i & 4) >> 2);
+            if (0 < (odd != 0 ? 1.0f : -1.0f) * mat[5 + dir]) continue;
+            int origin_index = (1 << ((dir - 1) % 3));
+            int right_index = (1 << ((dir + odd) % 3)) | origin_index;
+            int down_index = (1 << ((dir + (odd ^ 1)) % 3)) | origin_index;
+            if (odd != 0)
+            {
+                origin_index ^= 7;
+                right_index ^= 7;
+                down_index ^= 7;
+            }
+            FPoint origin, right, down;
+            origin.X = x0 + px * corners[0 + 2 * origin_index];
+            origin.Y = y0 + px * corners[1 + 2 * origin_index];
+            right.X = x0 + px * corners[0 + 2 * right_index];
+            right.Y = y0 + px * corners[1 + 2 * right_index];
+            down.X = x0 + px * corners[0 + 2 * down_index];
+            down.Y = y0 + px * corners[1 + 2 * down_index];
+
+            RenderTextureAffine(renderer, texture, IntPtr.Zero, in origin, in right, in down);
+        }
+        
         RenderPresent(renderer);
 
         return AppResult.Continue;  // carry on with the program!
@@ -101,6 +180,7 @@ internal class Program
     // This function runs once at shutdown.
     static void AppQuit(nint appstate, AppResult result)
     {
+        DestroyTexture(texture);
         // SDL will clean up the window/renderer for us.
     }
 }
