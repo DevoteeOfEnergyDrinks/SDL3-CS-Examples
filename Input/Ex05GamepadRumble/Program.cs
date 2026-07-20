76dd848ea97fe71281e16c3c36cd282f66b8b88d
EnergyDrinkDevotee <EnergyDrinkDevotee@proton.me>
2026-07-20 21:23:46 +0200

    Finished InputEx05

--- Input/Ex05GamepadRumble/Program.cs
+++ Input/Ex05GamepadRumble/Program.cs
@@ -1,11 +1,8 @@
 ﻿/*
-* This example code creates an SDL window and renderer, and then clears the
-* window to a different color every frame, so you'll effectively get a window
-* that's smoothly fading between colors.
-*
-* This code is public domain. Feel free to use it for any purpose!
-* This code is a port of the official SDL3 examples
-*/
+ * This example rumbles gamepads on button presses.
+ *
+ * This code is public domain. Feel free to use it for any purpose!
+ */
 internal class Program
 {
     // These delegates map our C# methods to the internal SDL3 lifecycle events.
@@ -19,6 +16,28 @@ internal class Program
     // These variables hold the memory addresses of the window and the renderer.
     public static IntPtr window = IntPtr.Zero;
     public static IntPtr renderer = IntPtr.Zero;
+
+    public class GamepadInfo
+    {
+        public uint GamepadID;
+        public string? Action;
+    }
+
+    public static GamepadInfo[] gamepadsInfo = new GamepadInfo[16];
+
+
+    public static GamepadInfo? FindGamepadInfo(uint which)
+    {
+        int i;
+        for (i = 0; i < gamepadsInfo.Length; i++)
+        {
+            if (gamepadsInfo[i].GamepadID == which)
+            {
+                return gamepadsInfo[i];
+            }
+        }
+        return null;
+    }
 
 
     private static void Main(string[] args)
@@ -44,9 +63,9 @@ internal class Program
     // This function runs once at startup.
     static AppResult AppInit(ref nint appstate, int argc, string[]? argv)
     {
-        SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
+        SetAppMetadata("Example Input Gamepad Rumble", "1.0", "com.example.input-gamepad-rumble");
 
-        if (!Init(InitFlags.Video))
+        if (!Init(InitFlags.Video | InitFlags.Gamepad))
         {
             Log($"Couldn't initialize SDL: {GetError()}");
             return AppResult.Failure;
@@ -58,7 +77,11 @@ internal class Program
             return AppResult.Failure;
         }
 
-        SetRenderLogicalPresentation(renderer, 640, 480, RendererLogicalPresentation.Letterbox);
+        // Initialize each slot with a unique new object
+        for (int i = 0; i < gamepadsInfo.Length; i++)
+        {
+            gamepadsInfo[i] = new GamepadInfo { GamepadID = 0, Action = "idle" };
+        }
 
         return AppResult.Continue;  // carry on with the program!
     }
@@ -67,11 +90,77 @@ internal class Program
     // This function runs when a new event (mouse input, keypresses, etc) occurs.
     static AppResult AppEvent(nint appstate, ref Event evt)
     {
+        GamepadInfo? gamepadInfo;
         if (evt.Type == (uint)EventType.Quit)
         {
             return AppResult.Success;   // end the program, reporting success to the OS.
         }
+        else if (evt.Type == (uint)EventType.GamepadAdded)
+        {   // this event is sent for each hotplugged stick, but also each already-connected gamepad during SDL_Init().
+            OpenGamepad(evt.GDevice.Which);
+            gamepadInfo = FindGamepadInfo(0);  // find an empty space
+            if (gamepadInfo != null)
+            {
+                gamepadInfo.GamepadID = evt.GDevice.Which;
+                gamepadInfo.Action = "idle";
+            }
+        }
+        else if (evt.Type == (uint)EventType.GamepadRemoved)
+        {
+            IntPtr gamepadPtr = GetGamepadFromID(evt.GDevice.Which);
+            CloseGamepad(gamepadPtr);
+            gamepadInfo = FindGamepadInfo(evt.GDevice.Which);
+            if (gamepadInfo != null)
+            {
+                gamepadInfo.GamepadID = 0;
+            }
+        }
+        else if (evt.Type == (uint)EventType.GamepadButtonDown)
+        {
+            IntPtr gamepadPtr = GetGamepadFromID(evt.GButton.Which);
+            gamepadInfo = FindGamepadInfo(evt.GButton.Which);
+            switch (evt.GButton.Button)
+            {
+                case (byte)GamepadButton.South:
+                    RumbleGamepad(gamepadPtr, 0xFFFF, 0x0000, 5000);
+                    if (gamepadInfo != null)
+                    {
+                        gamepadInfo.Action = "rumble high frequency";
+                    }
+                    break;
+                case (byte)GamepadButton.East:
+                    RumbleGamepad(gamepadPtr, 0x0000, 0xFFFF, 5000);
+                    if (gamepadInfo != null)
+                    {
+                        gamepadInfo.Action = "rumble low frequency";
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        else if (evt.Type == (uint)EventType.GamepadButtonUp)
+        {
+            IntPtr gamepadPtr = GetGamepadFromID(evt.GButton.Which);
+            RumbleGamepad(gamepadPtr, 0x0000, 0x0000, 0);
+            gamepadInfo = FindGamepadInfo(evt.GButton.Which);
+            if (gamepadInfo != null)
+            {
+                gamepadInfo.Action = "idle";
+            }
+        }
         return AppResult.Continue;  // carry on with the program!
+    }
+
+
+    static void DrawCenteredText(int renderWidth, ref int y, string text)
+    {
+        int x = (renderWidth - (text.Length * DebugTextFontCharacterSize)) / 2;
+        if (!string.IsNullOrEmpty(text))
+        {
+            RenderDebugText(renderer, x, y, text);
+        }
+        y += DebugTextFontCharacterSize * 2;
     }
 
 
@@ -79,19 +168,35 @@ internal class Program
     static AppResult AppIterate(nint appstate)
     {
         Delay(6);
-        double now = GetTicks() / 1000.0f;  // convert from milliseconds to seconds.
-
-        // choose the color for the frame we will draw. The sine wave trick makes it fade between colors smoothly.
-        float red = (float)(0.5f + 0.5f * Math.Sin(now));
-        float green = (float)(0.5f + 0.5f * Math.Sin(now + Math.PI * 2 / 3));
-        float blue = (float)(0.5f + 0.5f * Math.Sin(now + Math.PI * 4 / 3));
-
-        SetRenderDrawColorFloat(renderer, red, green, blue, 1.0f);  // new color, full alpha
-
-        // clear the window to the draw color.
+        int renderWidth;
+        int renderHeight;
+        int y;
+        int i;
+        GetCurrentRenderOutputSize(renderer, out renderWidth, out renderHeight);
+        SetRenderDrawColor(renderer, 0, 0, 0, 255);  // clear to black
         RenderClear(renderer);
 
-        // put the newly-cleared rendering on the screen.
+        y = DebugTextFontCharacterSize * 8;
+        SetRenderDrawColor(renderer, 255, 255, 0, 255);  // yellow text
+        DrawCenteredText(renderWidth, ref y, "Connect gamepads and press buttons to rumble.");
+        y += DebugTextFontCharacterSize * 3;
+
+        // report all the visible joysticks and what they are doing at the moment.
+        SetRenderDrawColor(renderer, 255, 255, 255, 255);  // white text
+        for (i = 0; i < gamepadsInfo.Length; i++)
+        {
+            uint which = gamepadsInfo[i].GamepadID;
+            if (which == 0)
+            {
+                DrawCenteredText(renderWidth, ref y, "");  // just leave a blank line.
+            }
+            else
+            {
+                string text = $"{GetGamepadNameForID(which)}: {gamepadsInfo[i].Action}";
+                DrawCenteredText(renderWidth, ref y, text);
+            }
+        }
+
         RenderPresent(renderer);
 
         return AppResult.Continue;  // carry on with the program!
@@ -101,6 +206,7 @@ internal class Program
     // This function runs once at shutdown.
     static void AppQuit(nint appstate, AppResult result)
     {
+        Quit();
         // SDL will clean up the window/renderer for us.
     }
 }
