0e2df81ce2e2943d44f4d00fd36a32029e803298
EnergyDrinkDevotee <EnergyDrinkDevotee@proton.me>
2026-07-21 14:57:31 +0200

    Finished AudioEx03

--- Audio/Ex03LoadWav/Program.cs
+++ Audio/Ex03LoadWav/Program.cs
@@ -1,11 +1,16 @@
 ﻿/*
-* This example code creates an SDL window and renderer, and then clears the
-* window to a different color every frame, so you'll effectively get a window
-* that's smoothly fading between colors.
-*
-* This code is public domain. Feel free to use it for any purpose!
-* This code is a port of the official SDL3 examples
-*/
+ * This example code creates a simple audio stream for playing sound, and
+ * loads a .wav file that is pushed through the stream in a loop.
+ *
+ * This code is public domain. Feel free to use it for any purpose!
+ *
+ * The .wav file is from this site https://mixkit.co/free-stock-music/
+ *
+ *      Song:   Tech House vibes
+ *      by      Alejandro Magaña (A. M.)
+ *      converted from mp3 to wav
+ */
+using System.Runtime.InteropServices;
 internal class Program
 {
     // These delegates map our C# methods to the internal SDL3 lifecycle events.
@@ -19,7 +24,9 @@ internal class Program
     // These variables hold the memory addresses of the window and the renderer.
     public static IntPtr window = IntPtr.Zero;
     public static IntPtr renderer = IntPtr.Zero;
-
+    public static IntPtr stream = IntPtr.Zero;
+    public static IntPtr wavData;
+    public static uint wavDataLen = 0;
 
     private static void Main(string[] args)
     {
@@ -44,21 +51,45 @@ internal class Program
     // This function runs once at startup.
     static AppResult AppInit(ref nint appstate, int argc, string[]? argv)
     {
-        SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
+        AudioSpec spec;
+        string wavPath;
 
-        if (!Init(InitFlags.Video))
+        SetAppMetadata("Example Audio Load Wave", "1.0", "com.example.audio-load-wav");
+
+        if (!Init(InitFlags.Video | InitFlags.Audio))
         {
             Log($"Couldn't initialize SDL: {GetError()}");
             return AppResult.Failure;
         }
 
-        if (!CreateWindowAndRenderer("examples/renderer/clear", 640, 480, WindowFlags.Resizable, out window, out renderer))
+        // we don't _need_ a window for audio-only things but it's good policy to have one.
+        if (!CreateWindowAndRenderer("examples/audio/load-wav", 640, 480, WindowFlags.Resizable, out window, out renderer))
         {
             Log($"Couldn't create window/renderer: {GetError()}");
             return AppResult.Failure;
         }
 
         SetRenderLogicalPresentation(renderer, 640, 480, RendererLogicalPresentation.Letterbox);
+
+        // Load the .wav file from wherever the app is being run from.
+        wavPath = GetBasePath() + "Assets/sample.wav";  // build the string of the full file path
+
+        if (!LoadWAV(wavPath, out spec, out wavData, out wavDataLen))
+        {
+            Log($"Couldn't load .wav file: {GetError()}");
+            return AppResult.Failure;
+        }
+
+        // Create our audio stream in the same format as the .wav file. It'll convert to what the audio hardware wants.
+        stream = OpenAudioDeviceStream(AudioDeviceDefaultPlayback, in spec, null, IntPtr.Zero);
+        if (stream == IntPtr.Zero)
+        {
+            Log($"Couldn't create audio stream: {GetError()}");
+            return AppResult.Failure;
+        }
+
+        // SDL_OpenAudioDeviceStream starts the device paused. You have to tell it to start!
+        ResumeAudioStreamDevice(stream);
 
         return AppResult.Continue;  // carry on with the program!
     }
@@ -79,21 +110,19 @@ internal class Program
     static AppResult AppIterate(nint appstate)
     {
         Delay(6);
-        double now = GetTicks() / 1000.0f;  // convert from milliseconds to seconds.
+        // see if we need to feed the audio stream more data yet.
+        // We're being lazy here, but if there's less than the entire wav file left to play,
+        // just shove a whole copy of it into the queue, so we always have _tons_ of
+        // data queued for playback.
+        if (GetAudioStreamQueued(stream) < (int)wavDataLen)
+        {
+            // feed more data to the stream. It will queue at the end, and trickle out as the hardware needs more data.
+            PutAudioStreamData(stream, wavData, (int)wavDataLen);
+        }
 
-        // choose the color for the frame we will draw. The sine wave trick makes it fade between colors smoothly.
-        float red = (float)(0.5f + 0.5f * Math.Sin(now));
-        float green = (float)(0.5f + 0.5f * Math.Sin(now + Math.PI * 2 / 3));
-        float blue = (float)(0.5f + 0.5f * Math.Sin(now + Math.PI * 4 / 3));
-
-        SetRenderDrawColorFloat(renderer, red, green, blue, 1.0f);  // new color, full alpha
-
-        // clear the window to the draw color.
+        // we're not doing anything with the renderer, so just blank it out.
         RenderClear(renderer);
-
-        // put the newly-cleared rendering on the screen.
         RenderPresent(renderer);
-
         return AppResult.Continue;  // carry on with the program!
     }
 
@@ -101,6 +130,7 @@ internal class Program
     // This function runs once at shutdown.
     static void AppQuit(nint appstate, AppResult result)
     {
+        Free(wavData);  // strictly speaking, this isn't necessary because the process is ending, but it's good policy.
         // SDL will clean up the window/renderer for us.
     }
 }
